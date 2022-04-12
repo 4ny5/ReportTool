4c136b70a06af79689122039b3267965fc42bf41
George <33947264+Georgie045@users.noreply.github.com>
2022-04-13 06:23:13 +1000

    first commit

---
+++ ReportTool.py
@@ -0,0 +1,28 @@
+import requests, os, time
+from requests.sessions import session
+import colorama
+from colorama import Fore
+colorama.init()
+
+session = requests.session()
+
+print(Fore.MAGENTA + """
+██████╗ ███████╗██████╗  █████╗ ██████╗ ████████╗████████╗ █████╗ █████╗  ██╗        ██████╗ ██╗   ██╗
+██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝╚══██╔══╝██╔══██╗██╔══██╗██║        ██╔══██╗╚██╗ ██╔╝
+██████╔╝█████╗  ██████╔╝██║  ██║██████╔╝   ██║      ██║   ██║  ██║██║  ██║██║        ██████╔╝ ╚████╔╝
+██╔══██╗██╔══╝  ██╔═══╝ ██║  ██║██╔══██╗   ██║      ██║   ██║  ██║██║  ██║██║        ██╔═══╝   ╚██╔╝
+██║  ██║███████╗██║     ╚█████╔╝██║  ██║   ██║      ██║   ╚█████╔╝╚█████╔╝███████╗██╗██║        ██║
+╚═╝  ╚═╝╚══════╝╚═╝      ╚════╝ ╚═╝  ╚═╝   ╚═╝      ╚═╝    ╚════╝  ╚════╝ ╚══════╝╚═╝╚═╝        ╚═╝
+""")
+
+x = input('Enter Url: ')
+print("Success!")
+time.sleep(3)
+
+os.system("cls")
+
+
+while True:
+    Post = session.post(x)
+    print(" Successfuly Sent Report " + Post.text)
+input()
---
+++ Start.bat
@@ -0,0 +1,3 @@
+@echo off 
+python ReportTool.py
+pause
